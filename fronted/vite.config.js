99b683b1655128b1540975813aafb1a839489335
Co-D-E-R <zh4223828@gmail.com>
2025-03-01 00:39:13 +0530

    commit: update vercel.json

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -2,19 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(
-  {
-    plugins: [react()],
-    build: {
-      outDir:'dist'
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, 'api')
-        }
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, 'api')
       }
     }
-  });
+  }
+});
